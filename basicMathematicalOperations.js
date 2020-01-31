0b4155bdc4487145a35ae018fe2fc868394ebb0f
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-01-31 09:15:54 +0000

    add basicMathematicalOperations.js

---
+++ basicMathematicalOperations.js
@@ -0,0 +1,57 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+  if( operation == "+") {
+    return value1 + value2
+  }
+  if( operation == "-") {
+    return value1 - value2
+  }
+  if( operation == "*") {
+    return value1 * value2
+  }
+  if( operation == "/") {
+    return value1 / value2
+  }
+}
+
+// function basicOp(operation, value1, value2){
+//     const operator = {
+//         "+": value1 + value2,
+//         "-": value1 - value2,
+//         "/": value1 / value2,
+//         "*": value1 * value2
+//     };
+
+//     return operator[operation];
+// };
+
+// function basicOp(operation, value1, value2) {
+//     switch (operation) {
+//         case '+': return value1 + value2;
+//         case '-': return value1 - value2;
+//         case '*': return value1 * value2;
+//         case '/': return value1 / value2;
+//         default: return 'Operation must be one of + - * /';
+//     }
+// }
+
+// function basicOp(operation, value1, value2) {
+//   var cases = {
+//     '+': value1 + value2,
+//     '-': value1 - value2,
+//     '*': value1 * value2,
+//     '/': value1 / value2
+//   };
+//   return cases[operation]
+// }
+
+console.log(basicOp('+', 4, 7)) //, 11);
+console.log(basicOp('-', 15, 18)) //, -3);
+console.log(basicOp('*', 5, 5)) //, 25);
+console.log(basicOp('/', 49, 7)) //, 7);
